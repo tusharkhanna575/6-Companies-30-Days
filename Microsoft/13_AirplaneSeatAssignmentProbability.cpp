a6fd55807725052d205ce97d7d508e5b546b90b9
Tushar Khanna <0808cs201190.ies@ipsacademy.org>
2023-01-05 00:15:32 +0530

    Create 13_AirplaneSeatAssignmentProbability.cpp

---
+++ Microsoft/13_AirplaneSeatAssignmentProbability.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        if(n==1) {
+            return(1.0);
+        }
+        else {
+            return(0.5);
+        }
+    }
+};
+
+/*
+    Time Complexity : O(1)
+    Space Complexity : O(1)
+*/
