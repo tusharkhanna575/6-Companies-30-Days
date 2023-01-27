281a7e03f8be373fca1d81504dd84ce162ee481c
Tushar Khanna <0808cs201190.ies@ipsacademy.org>
2023-01-27 14:47:06 +0530

    Create 13 Top K Frequent Words.py

---
+++ Amazon/13 Top K Frequent Words.py
@@ -0,0 +1,10 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        c = Counter(words) 
+        ans = sorted(c, key = lambda x: (-c[x],x))
+        return ans[:k]
+    
+"""
+    Time Complexity : O(n*logn)
+    Space Complexity : O(n)
+"""
