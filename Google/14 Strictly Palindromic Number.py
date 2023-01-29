d80085c939f6fbfada0aafe4e370a4b3948d61c0
Tushar Khanna <0808cs201190.ies@ipsacademy.org>
2023-01-29 23:24:50 +0530

    Create 14 Strictly Palindromic Number.py

---
+++ Google/14 Strictly Palindromic Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        pass
+     
+    
+"""
+    Time Complexity : O(0)
+    Space Complexity : O(0)
+    
+    
+    P.S. -> Just for fun!
+"""    
+    
